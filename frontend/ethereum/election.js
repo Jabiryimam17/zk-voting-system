e1ac9c1ddf287e22f4b8a1810c47eee742fee6e5
Jabir Yimam <jabiryimam1@gmail.com>
2026-07-15 11:07:19 +0300

    config missing fixed

--- frontend/ethereum/election.js
+++ frontend/ethereum/election.js
@@ -8,7 +8,8 @@ export default function contact_interface(contract_address) {
   return new ethers.Contract(contract_address, abi, ether);
 }
 
-import { election_host } from "../config";
+const election_host =
+  process.env.NEXT_PUBLIC_ELECTION_HOST || "http://localhost:8080";
 
 export async function election_contract() {
   const response = await axios.get(
